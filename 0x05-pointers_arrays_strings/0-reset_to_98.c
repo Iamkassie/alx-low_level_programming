90aeef15c85d1559fb226db0bab3724e4784cbab
opeyemi <kassyore7@gmail.com>
2022-09-19 15:27:41 -0700

    My reset98 commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,11 @@
 
 /**
  * reset_to_98 - a function that takes a pointer and updates its value
- *
+ * &n: pointer
  * Return: void
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
