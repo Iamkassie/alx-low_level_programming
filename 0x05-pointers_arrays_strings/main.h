4aa94655ebd023bee14d9171f63a7143776f2575
opeyemi <kassyore7@gmail.com>
2022-09-20 10:23:41 -0700

    My reset98 commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
