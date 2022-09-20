7ec4990080045770a343981d6770c3a80fc064db
opeyemi <kassyore7@gmail.com>
2022-09-20 14:18:35 -0700

    My print array commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - prints n of elements of int arrays
+ * @a: input array
+ * @n: input n element
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
