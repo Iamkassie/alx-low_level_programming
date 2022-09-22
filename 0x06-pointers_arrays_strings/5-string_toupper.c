f571fde7f55bf42648113ae00613adf5951b53b5
opeyemi <kassyore7@gmail.com>
2022-09-22 16:30:39 -0700

    My toupper commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,14 +6,14 @@
  * Return: pointer to dest
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
 	int count = 0;
 
 	while (*(s + count) != '\0')
 	{
 		if ((*(s + count) >= 97) && (*(s + count) <= 122))
-			*(s + count) = *(s + count) - 33;
+			*(s + count) = *(s + count) - 32;
 		count++;
 	}
 
