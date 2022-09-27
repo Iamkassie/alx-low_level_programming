ed7b0d35aca5a1b0393b3a82e6951da5d973a37a
opeyemi <kassyore7@gmail.com>
2022-09-27 15:19:17 -0700

    My memcpy commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _memcpy.c - irontnoux2
+* @dest: pointer to char params
+* @src: pointer tp char params
+* @n: size
+* Return: *dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest{i} = src{i};
+	}
+
+	return (dest);
+}
