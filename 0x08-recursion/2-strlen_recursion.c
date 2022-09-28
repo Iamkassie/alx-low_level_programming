d29efe249f5927ee71a8a8066053fd255ed5e09c
opeyemi <kassyore7@gmail.com>
2022-09-28 16:53:13 -0700

    My strlen recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,10 +9,9 @@
 
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
-	if (*s)
+	if (!*s)
 	{
-		len++;
-		len+- _strlen_recursion(s + 1);
+		return (0);
 	}
+	return (1 + _strlen_recursion(++s));
+}
